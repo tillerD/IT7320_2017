0926aeb143561d8da61f0dece57a19616c9fd6e4
student <student@Win7SP1x64VM.localdomain>
2017-08-09 17:25:38 +1200

    Removed main from login
    - Luke

--- Alumni/src/version1/LogIn.java
+++ Alumni/src/version1/LogIn.java
@@ -103,8 +103,8 @@ public class LogIn extends JFrame {
 		
 
 	}
-	
-	public void main(String args[])
+
+	public static void main(String args[])
 	{
 		LogIn loginScreen = new LogIn();
 		loginScreen.setVisible(true);
