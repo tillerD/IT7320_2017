0cb51a37fee0157cf9460ea8ee50ae1ff6d09cff
student <student@Win7SP1x64VM.localdomain>
2017-08-16 20:19:58 +1200

    Fixed signup screen
    -Dylan Tiller

--- Alumni/src/version1/SignUp.java
+++ Alumni/src/version1/SignUp.java
@@ -1,13 +1,20 @@
 package version1;
 
-import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import java.awt.Dimension;
 
 @SuppressWarnings("serial")
 public class SignUp extends JFrame {
@@ -17,7 +24,15 @@ public class SignUp extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
+	private JTextField username;
+	private JTextField password;
+	private JTextField cpassword;
+	private static String URL = "jdbc:mysql://localhost:3306/alumnischema";
+	private static final String USER = "AlumniAdmin";
+	private static final String PASSWORD = "WelTec123";
+	
 	public SignUp() {
+		setMinimumSize(new Dimension(350, 350));
 		setTitle("WelTec Alumni");
 		getContentPane().setLayout(null);
 
@@ -25,162 +40,134 @@ public class SignUp extends JFrame {
 		lblFirstName.setBounds(10, 11, 77, 14);
 		getContentPane().add(lblFirstName);
 
-		JLabel lblErrorFN = new JLabel("Field Required");
-		lblErrorFN.setForeground(Color.RED);
-		lblErrorFN.setBounds(283, 11, 151, 14);
-		getContentPane().add(lblErrorFN);
-		lblErrorFN.setVisible(false);
-
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setBounds(10, 36, 77, 14);
 		getContentPane().add(lblLastName);
 
-		JLabel lblErrorLN = new JLabel("Field Required");
-		lblErrorLN.setForeground(Color.RED);
-		lblErrorLN.setBounds(283, 36, 151, 14);
-		getContentPane().add(lblErrorLN);
-		lblErrorLN.setVisible(false);
-
 		JLabel lblStudnetId = new JLabel("Studnet ID");
-		lblStudnetId.setBounds(10, 61, 77, 14);
+		lblStudnetId.setBounds(10, 61, 164, 14);
 		getContentPane().add(lblStudnetId);
-
-		JLabel lblErrorSID = new JLabel("Field Required");
-		lblErrorSID.setForeground(Color.RED);
-		lblErrorSID.setBounds(283, 61, 151, 14);
-		getContentPane().add(lblErrorSID);
-		lblErrorSID.setVisible(false);
 
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setBounds(10, 86, 46, 14);
 		getContentPane().add(lblEmail);
 
-		JLabel lblErrorEmail = new JLabel("Field Required");
-		lblErrorEmail.setForeground(Color.RED);
-		lblErrorEmail.setBounds(283, 86, 151, 14);
-		getContentPane().add(lblErrorEmail);
-		lblErrorEmail.setVisible(false);
-
 		JLabel lblConfirmEmail = new JLabel("Confirm Email");
-		lblConfirmEmail.setBounds(10, 111, 77, 14);
+		lblConfirmEmail.setBounds(10, 111, 186, 14);
 		getContentPane().add(lblConfirmEmail);
-
-		JLabel lblErrorConfirmEmail = new JLabel("Field Required");
-		lblErrorConfirmEmail.setForeground(Color.RED);
-		lblErrorConfirmEmail.setBounds(283, 111, 151, 14);
-		getContentPane().add(lblErrorConfirmEmail);
-		lblErrorConfirmEmail.setVisible(false);
 
 		JLabel lblStudies = new JLabel("Studied");
 		lblStudies.setBounds(10, 136, 46, 14);
 		getContentPane().add(lblStudies);
 
-		JLabel lblErrorStudied = new JLabel("Field Required");
-		lblErrorStudied.setForeground(Color.RED);
-		lblErrorStudied.setBounds(283, 136, 151, 14);
-		getContentPane().add(lblErrorStudied);
-		lblErrorStudied.setVisible(false);
-
 		textField = new JTextField();
 		textField.setColumns(45);
-		textField.setBounds(97, 8, 176, 20);
+		textField.setBounds(149, 11, 176, 20);
 		getContentPane().add(textField);
 
 
 		textField_1 = new JTextField();
 		textField_1.setColumns(45);
-		textField_1.setBounds(97, 33, 176, 20);
+		textField_1.setBounds(149, 36, 176, 20);
 		getContentPane().add(textField_1);
 
 		textField_2 = new JTextField();
 		textField_2.setColumns(45);
-		textField_2.setBounds(97, 58, 176, 20);
+		textField_2.setBounds(149, 61, 176, 20);
 		getContentPane().add(textField_2);
 
 		textField_3 = new JTextField();
 		textField_3.setColumns(45);
-		textField_3.setBounds(97, 83, 176, 20);
+		textField_3.setBounds(149, 86, 176, 20);
 		getContentPane().add(textField_3);
 
 		textField_4 = new JTextField();
 		textField_4.setColumns(45);
-		textField_4.setBounds(97, 108, 176, 20);
+		textField_4.setBounds(149, 111, 176, 20);
 		getContentPane().add(textField_4);
 
 		textField_5 = new JTextField();
 		textField_5.setColumns(45);
-		textField_5.setBounds(97, 133, 176, 20);
+		textField_5.setBounds(149, 136, 176, 20);
 		getContentPane().add(textField_5);
 
 
 		JButton btnSignUp = new JButton("Sign Up");
 		btnSignUp.addMouseListener(new MouseAdapter() {
-			@SuppressWarnings("unlikely-arg-type")
 			@Override
 			//Check fields aren't empty and meet the requirements for each filed
 			public void mouseClicked(MouseEvent e) {
 
 				if(textField.getText().equals("") && textField_1.getText().equals("") && textField_2.getText().equals("") && textField_3.getText().equals("") && textField_4.getText().equals("") && textField.getText().equals("")){
-					lblErrorFN.setVisible(true);
-					lblErrorFN.setText("Field Required");
-					lblErrorLN.setVisible(true);
-					lblErrorLN.setText("Field Required");
-					lblErrorSID.setVisible(true);
-					lblErrorSID.setText("Field Required");
-					lblErrorEmail.setVisible(true);
-					lblErrorEmail.setText("Field Required");
-					lblErrorConfirmEmail.setVisible(true);
-					lblErrorConfirmEmail.setText("Field Required");
-					lblErrorStudied.setVisible(true);
-					lblErrorStudied.setText("Field Required");
+					JOptionPane.showMessageDialog(new JFrame(), "All Fields Required!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 				}
 				else if(textField.getText().matches("[0-9]*") && textField_1.getText().matches("[0-9]*")){
-					lblErrorFN.setVisible(true);
-					lblErrorFN.setText("Not a Name");
-					lblErrorLN.setVisible(true);
-					lblErrorLN.setText("Not a Name");
+					JOptionPane.showMessageDialog(new JFrame(), "First and Last Name must not contain numbers!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 					
 				}
 				else if(textField_2.getText().matches("[a-z A-Z]*")) {
-					lblErrorSID.setVisible(true);
-					lblErrorSID.setText("ID contains only numbers");
+					JOptionPane.showMessageDialog(new JFrame(), "Student ID must only contsain letters!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 				}
 				else if(textField_2.getText().length() < 7 || textField_2.getText().length() > 7 ) {
-					lblErrorSID.setVisible(true);
-					lblErrorSID.setText("Incorrect amount of # ");
+					JOptionPane.showMessageDialog(new JFrame(), "Length of Student ID must be 7 numbers!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 				}
 				else if((textField_3.getText().trim().contains("@" + "."))){
-					lblErrorEmail.setText("Not a proper email address");
-					lblErrorEmail.setVisible(true);
+					JOptionPane.showMessageDialog(new JFrame(), "Invalid Email!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 				}
 				else if(!textField_3.getText().equals(textField_4.getText())) {
-					lblErrorEmail.setText("Emails do not match");
-					lblErrorEmail.setVisible(true);
+					JOptionPane.showMessageDialog(new JFrame(), "Emails do not match!", "Check Fields", JOptionPane.ERROR_MESSAGE);
 				}
-
+				else if(username.getText().length() >= 40)
+				{
+					JOptionPane.showMessageDialog(new JFrame(), "Username must not be longer than 40 characters!", "Check Fields", JOptionPane.ERROR_MESSAGE);
+				}
+				else if(password.getText().length() >= 40)
+				{
+					JOptionPane.showMessageDialog(new JFrame(), "Password must not be longer than 40 characters!", "Check Fields", JOptionPane.ERROR_MESSAGE);
+				}
+				else if(!cpassword.getText().equals(password.getText()))
+				{
+					JOptionPane.showMessageDialog(new JFrame(), "Passwords do not match!", "Check Fields", JOptionPane.ERROR_MESSAGE);
+				}
+				
 				else {
-					System.out.println("Batman");
-					/*View viewScreen = new View();
-					viewScreen.setVisible(true);
-					dispose();*/
+					String query = "INSERT INTO alumnischema.students " + 
+							"(idStudents, Username, Password, firstName, LastName, StudentID, Email,  Admin)" + 
+							" VALUES (3, '" + username.getText() + "', '" + password.getText() + "', '" + textField.getText() + "', '" + textField_1.getText() + "', " + Integer.parseInt(textField_2.getText()) + ", '" + textField_3.getText() + "', " + "0);";
+					Connection connection = null;
+					try {
+						connection = DriverManager.getConnection(URL, USER, PASSWORD);
+						PreparedStatement pstmt = (PreparedStatement) connection.prepareStatement(query);
+						pstmt.executeUpdate();
+						pstmt.close();
+						JOptionPane.showMessageDialog(new JFrame(), "SignUp Complete!", "SignUp", JOptionPane.ERROR_MESSAGE);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} finally {
+						if (connection != null) {
+							try {
+								connection.close();
+							} catch (SQLException e1) {
+								e1.printStackTrace();
+							}
+						}
+					}
+					
+					LogIn loginScreen = new LogIn();
+					loginScreen.setVisible(true);
+					dispose();
 				}
 			}
 		});
-		btnSignUp.setBounds(85, 164, 89, 23);
+		btnSignUp.setBounds(137, 278, 89, 23);
 		getContentPane().add(btnSignUp);
 
 
 		JButton btnClear = new JButton("Clear");
 		btnClear.addMouseListener(new MouseAdapter() {
-			@SuppressWarnings("unlikely-arg-type")
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				lblErrorFN.setVisible(false);
-				lblErrorLN.setVisible(false);
-				lblErrorSID.setVisible(false);
-				lblErrorEmail.setVisible(false);
-				lblErrorConfirmEmail.setVisible(false);
-				lblErrorStudied.setVisible(false);
 				textField.setText("");
 				textField_1.setText("");
 				textField_2.setText("");
@@ -189,14 +176,35 @@ public class SignUp extends JFrame {
 				textField_5.setText("");
 			}
 		});
-		btnClear.setBounds(184, 164, 89, 23);
+		btnClear.setBounds(236, 278, 89, 23);
 		getContentPane().add(btnClear);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(10, 164, 118, 14);
+		getContentPane().add(lblUsername);
+		
+		username = new JTextField();
+		username.setColumns(45);
+		username.setBounds(149, 164, 176, 20);
+		getContentPane().add(username);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(10, 192, 129, 14);
+		getContentPane().add(lblPassword);
+		
+		password = new JTextField();
+		password.setColumns(45);
+		password.setBounds(149, 192, 176, 20);
+		getContentPane().add(password);
+		
+		JLabel lblConfirmPassword = new JLabel("Confirm Password");
+		lblConfirmPassword.setBounds(10, 220, 118, 14);
+		getContentPane().add(lblConfirmPassword);
+		
+		cpassword = new JTextField();
+		cpassword.setColumns(45);
+		cpassword.setBounds(149, 220, 176, 20);
+		getContentPane().add(cpassword);
 
-	}
-
-	public static void main(String args[])
-	{
-		SignUp signUpScreen = new SignUp();
-		signUpScreen.setVisible(true);
 	}
 }
